b5466919cd1c63c34ffbed5b5f1cc185d8e6513f
Clément Schreiner <clement@mux.me>
2022-01-19 17:13:29 +0100

    add simple dynamic app

--- app/api.py
+++ app/api.py
@@ -1,9 +1,15 @@
+import datetime
+import json
+
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from tinydb import TinyDB
+
+from app import covid
 
 
 class PredictionData(BaseModel):
@@ -17,19 +23,27 @@ class Prediction(BaseModel):
     data: PredictionData
 
 
-templates = Jinja2Templates(directory='website/predictions/')
+templates = Jinja2Templates(directory='website/')
 
 
-async def homepage(request):
+async def predictions_homepage(request):
     predictions = [Prediction.parse_obj(d) for d in request.app.state.db.all()]
-    return templates.TemplateResponse('index.html', {
+    return templates.TemplateResponse('predictions/index.html', {
         'request': request,
         'predictions': predictions,
     })
 
 
+async def homepage(request):
+    return templates.TemplateResponse('charts/index.html', {
+        'request': request,
+        'title': 'Covid charts',
+    })
+
+
 app = FastAPI(routes=[
-    Route('/', homepage),
+    Route('/predictions/index.html', predictions_homepage),
+    Route('/index.html', homepage),
     Mount('/static', StaticFiles(directory='website/static'), name='static')
 ])
 app.state.db = TinyDB('db.json')
@@ -50,3 +64,10 @@ async def get_predictions():
     return {
         'predictions': predictions,
     }
+
+@app.get('/data')
+async def get_data(start: str):
+    start_date = datetime.datetime.strptime(start, '%Y-%m-%d')
+    data: pd.DataFrame = covid.get_latest_data(start_date=start_date)
+    data_dict = json.loads(data.to_json(date_format='epoch'))
+    return data_dict
