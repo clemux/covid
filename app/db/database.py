7e003cbac951e12c93e6f9ef68cc8cf826d98795
Clément Schreiner <clement@mux.me>
2022-04-03 11:45:47 +0200

    add env variable for database url

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.environ['COVID_DATABASE_URL']
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
